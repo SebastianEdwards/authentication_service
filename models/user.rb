3fcc72eb9e313c1136da4d8236473433b6f3739f
Sebastian Edwards <s.thursday@gmail.com>
2012-02-29 18:52:45 +1300

    Added user model.

---
+++ models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  has_many :authorizations
+  has_secure_password
+  attr_accessible :email,
+                  :password,
+                  :provider_uid,
+                  :provider_type,
+                  :authenticatable_id,
+                  :authenticatable_type
+
+  validates :email, :password, presence:
+    {unless: -> {provider_uid && provider_type}}
+  validates :provider_uid, :provider_type, presence:
+    {unless: -> {email && password}}
+end
