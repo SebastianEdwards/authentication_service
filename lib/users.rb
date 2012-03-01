60bc21e9303d0163874a72698aac9808365c3b4c
Sebastian Edwards <s.thursday@gmail.com>
2012-03-01 16:51:04 +1300

    Refactor users create endpoint.

--- lib/users.rb
+++ lib/users.rb
@@ -17,14 +17,26 @@ module Users
   end
 
   class Create < Goliath::API
-    def response(env)
-      user = User.new(params)
-      user.save!
-      forwardable_params = params.select do |k,v|
+    def build_code
+      code = SecureRandom.hex(8)
+      REDIS.set "code:#{code}",
+        {user_id: @user.id, client_id: 1}.to_json
+      code
+    end
+
+    def forwardable_params
+      params.select do |k,v|
         !%w{email password password_confirmation client_id redirect_uri}\
           .include? k
       end
-      [200, {'Content-Type' => 'application/JSON'}, forwardable_params.to_json]
+    end
+
+    def response(env)
+      @user = User.create!(params)
+      query = Rack::Utils.build_query \
+        forwardable_params.merge({code: build_code})
+      redirect_url = params[:redirect_uri] + '?' + query
+      [200, {'Content-Type' => 'application/JSON'}, redirect_url]
     end
   end
 end
