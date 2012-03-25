bc24e36002d3827e353c16234b1d231ea5843b2e
Sebastian Edwards <s.thursday@gmail.com>
2012-03-25 22:22:02 +1300

    Removing user validations.

--- models/user.rb
+++ models/user.rb
@@ -1,17 +1,4 @@
 class User < RedisModel
   namespace :user
   id_type :sequential
-
-  def valid?
-    valid_via_email? || valid_via_provider?
-  end
-
-  private
-  def valid_via_email?
-    attributes.has_key?('email') && attributes.has_key?('password_digest')
-  end
-
-  def valid_via_provider?
-    attributes.has_key?('provider_uid') && attributes.has_key?('provider_type')
-  end
 end
