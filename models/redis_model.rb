e7e1a8b70c5227defc2d4ce7e85bff40bcb9b2fa
Sebastian Edwards <s.thursday@gmail.com>
2012-03-25 21:00:02 +1300

    Adding ID generating functionality to RedisModel.

--- models/redis_model.rb
+++ models/redis_model.rb
@@ -1,36 +1,67 @@
 class RedisModel
   attr_accessor :attributes
 
-  def self.find(key)
-    json = REDIS.get("#{@namespace}:#{key}")
-    attributes = JSON.parse(json)
-    self.new(attributes, :new_record => false)
+  def self.find(id)
+    if json = REDIS.get("#{namespace}:#{id}")
+      attributes = JSON.parse(json)
+      self.new(attributes, id)
+    end
   end
 
-  def self.namespace(namespace)
-    @namespace = namespace
-  end
-
-  def self._namespace
+  def self.namespace(namespace = nil)
+    @namespace = namespace if namespace
     @namespace
   end
 
-  def initialize(attributes = {}, key = nil)
+  def namespace
+    self.class.namespace
+  end
+
+  def self.generate_id
+    send "generate_#{@id_type}_id"
+  end
+
+  def self.id_type(type)
+    if respond_to? "generate_#{type}_id"
+      @id_type = type
+    else
+      raise "Invalid ID type".inspect
+    end
+  end
+
+  def self.generate_sequential_id
+    REDIS.incr "#{namespace}:latest_id"
+  end
+
+  def self.generate_secure_random(length)
+    SecureRandom.hex(length)
+  end
+
+  def self.generate_secure_random_8_id
+    generate_secure_random(8)
+  end
+
+  def self.generate_secure_random_16_id
+    generate_secure_random(16)
+  end
+
+  def initialize(attributes = {}, id = nil)
     @attributes = attributes
-    @key = key
+    @id = id
   end
 
   def valid?; true; end
 
   def save
     if valid?
-      @key = REDIS.model_save [self.class._namespace, @key, attributes.to_json]
+      @id ||= self.class.generate_id
+      'OK' == REDIS.set("#{namespace}:#{@id}", attributes.to_json)
     else
-      raise "Error".inspect
+      raise "Invalid attributes".inspect
     end
   end
 
   def to_json
-    (@key ? attributes.merge(id: @key) : attributes).to_json
+    (@id ? attributes.merge(id: @id) : attributes).to_json
   end
 end
