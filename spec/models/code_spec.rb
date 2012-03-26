b4f246ffbca67c56c759a5f99057377310e40616
Sebastian Edwards <s.thursday@gmail.com>
2012-03-26 14:34:18 +1300

    Adding tests for code model.

--- spec/models/code_spec.rb
+++ spec/models/code_spec.rb
@@ -2,11 +2,22 @@ require './models/code'
 
 describe 'Code' do
   it 'should have namespace code' do
-    c = Code.new
-    c.namespace.should equal :code
+    Code.namespace.should equal :code
   end
 
   it 'should generate an 8 character id' do
     Code.generate_id.length.should equal 8
+  end
+
+  %w{client_id}.map(&:to_sym).each do |attribute|
+    it "should be not be valid if missing #{attribute}" do
+      code = Code.new
+      code.valid?.should_not be_true
+    end
+  end
+
+  it "should be valid when has client_id and user_id" do
+    code = Code.new(client_id: 1, user_id: 1)
+    code.valid?.should be_true
   end
 end
