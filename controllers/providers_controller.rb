34c4ad2694d9a63079bd4315d5555b94514929d2
Sebastian Edwards <s.thursday@gmail.com>
2012-03-26 14:40:07 +1300

    Updating validations on providers controller.

--- controllers/providers_controller.rb
+++ controllers/providers_controller.rb
@@ -1,37 +1,48 @@
-# require_relative './mixins/code_builder'
-
 module ProvidersController
   def self.included(base)
     base.get 'providers/:provider', Authorization
-    #  do
-    #   use Goliath::Rack::Validation::RequiredParam, :key => 'client_id'
-    #   use Goliath::Rack::Validation::RequiredParam, :key => 'redirect_uri'
-    # end
     base.get '/:provider/callback', Callback
   end
 
-  def unknown_provider
-    error = {error: 'Unknown provider.'}
-    [400, {'Content-Type' => 'application/JSON'}, error.to_json]
+  module CommonValidations
+    def self.included(base)
+      base.use Goliath::Rack::Validation::RequiredValue, {
+        :key => :provider,
+        :values => Provider.all.map(&:name)
+      }
+
+      %w{client_id redirect_uri}.each do |attribute|
+        base.use Goliath::Rack::Validation::RequiredParam, {
+          :key => attribute,
+          :message => 'query parameter missing.'
+        }
+      end
+    end
   end
 
   class Authorization < Goliath::API
+    include CommonValidations
+
     def response(env)
-      if provider = Provider[params[:provider]]
-        redirect = provider.authentication_url(env)
-        [301, {'Location' => redirect}]
-      else
-        unknown_provider
-      end
+      provider = Provider[params[:provider]]
+      redirect = provider.authentication_url(env)
+      [301, {'Location' => redirect}]
     end
   end # Authorization
 
   class Callback < Goliath::API
+    include CommonValidations
+
     def response(env)
       if provider = Provider[params[:provider]]
         uid = provider.uid(env)
-        user_id = REDIS.HGET(provider.name, uid)
-        user = (user_id ? User.find(user_id) : (u = User.new; u.save; u))
+        if user_id = REDIS.HGET(provider.name, uid)
+          user = User.find(user_id)
+        else
+          user = User.new
+          user.save
+          REDIS.HSET provider.name, uid, user.id
+        end
         code = Code.new({user_id: user.id, client_id: 1})
         code.save
         query = Rack::Utils.build_query({code: code.id})
