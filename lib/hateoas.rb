011efee60487de439bb40b6083b30df856afcb29
Sebastian Edwards <s.thursday@gmail.com>
2012-04-13 12:05:13 +1200

    Adding proper item and item data support as per Collection+JSON spec.

--- lib/hateoas.rb
+++ lib/hateoas.rb
@@ -1,9 +1,12 @@
 module HATEOAS
+  attr_reader :links, :items
+
   def initialize
     super
     @success_code = 200
     @headers = {'Content-Type' => 'application/vnd.collection+json'}
     @links = []
+    @items = []
   end
 
   def add_header(key, value)
@@ -14,28 +17,40 @@ module HATEOAS
     @success_code = code
   end
 
+  def add_item(href, data = [], links = [], opts = {}, &block)
+    if block_given?
+      item_data = Item.new(data, links)
+      yield(item_data)
+    end
+    href = ENV['ROOT_URI'] + href
+    @items << opts.merge({href: href, data: data})
+  end
+
+  class Item
+    attr_reader :data, :links
+
+    def initialize(data, links)
+      @data = data
+      @links = links
+    end
+
+    def add_data(name, value = '', prompt = '', opts = {})
+      item_data = opts.merge({name: name})
+      item_data.merge!({value: value}) if value != ''
+      item_data.merge!({prompt: prompt}) if prompt != ''
+      data << item_data
+    end
+  end
+
   def add_link(rel, href, opts = {})
     href = ENV['ROOT_URI'] + href
     @links << opts.merge({rel: rel, href: href})
   end
 
-  def generate_response(data = {})
-    content = Content.new(data, @links)
-    [@success_code, @headers, content.to_json]
-  end
-
-  class Content
-    def initialize(content, links)
-      @content = content
-      @links = links
-    end
-
-    def content
-      @content.respond_to?(:merge) ? @content : {items: @content}
-    end
-
-    def to_json
-      {collection: content.merge({links: @links})}.to_json
-    end
+  def generate_response
+    collection = {links: links}
+    collection.merge!({items: items}) if items.length > 0
+    response = { collection: collection }
+    [@success_code, @headers, response.to_json]
   end
 end
